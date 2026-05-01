9145e8cf0967d5d67246c440998bb04b33d0e2a5
maybelotus666 <sthefany.cmoreira@senacsp.edu.br>
2026-04-30 21:23:00 -0300

    dale

--- InstaFake/src/App.jsx
+++ InstaFake/src/App.jsx
@@ -8,8 +8,16 @@ function App() {
 
   return (
     <>
+    <section class="elipses">
+      <div class="elipse1"></div>
+      <div class="elipse2"></div>
+      <div class="elipse3"></div>
+      <div class="elipse4"></div>
+      <div class="elipse5"></div>
+    </section>
       <AddComentarios />
       <LikeButton />
+
     </>
   )
 }
