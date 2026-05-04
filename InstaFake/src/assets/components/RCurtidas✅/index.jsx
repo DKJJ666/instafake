b882f79ff593a487c01224707d5b809c90a26bbb
RyuuSenac <rafael.mgaldino@senacsp.edu.br>
2026-05-04 15:46:18 -0300

    Arrumando o botão de curtir para o css funcionar

--- InstaFake/src/assets/components/RCurtidas✅/index.jsx
+++ InstaFake/src/assets/components/RCurtidas✅/index.jsx
@@ -1,28 +1,28 @@
 import { useState } from 'react'
+import './style.css'
 import Heart from '../../../../public/heart1.svg'
 import HeartRed from '../../../../public/heartRed2.svg'
 
 export default function LikeButton() {
-
   const [like, setLike] = useState(229)
   const [liked, setLiked] = useState(false)
 
-  function admLike(){
-    if(!liked){
+  function admLike() {
+    if (!liked) {
       setLike(like + 1)
       setLiked(true)
-    }else{
+    } else {
       setLike(like - 1)
       setLiked(false)
     }
   }
 
   return (
-    <div>
-      <div style={{marginTop: "20px"}}>
-        {/* <p>{like}</p> */}
-        <button onClick={admLike}> <img src={liked ? HeartRed : Heart} alt="icon" width={25}/> {like} </button>
-      </div>
+    <div className="like">
+      <button className={`like-btn${liked ? ' liked' : ''}`} onClick={admLike}>
+        <img src={liked ? HeartRed : Heart} alt="icon" width={25} />
+        {like}
+      </button>
     </div>
   )
 }
